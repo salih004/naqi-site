9c92895da69f6fe34fbf6fda543163ef053f97ea
salih004 <145712148+salih004@users.noreply.github.com>
2026-01-17 22:21:58 -0600

    configuration issues fix

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/naqi-site/", // <- This fixes all absolute paths
   plugins: [react()],
 });
