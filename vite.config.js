dd7fea9824709f44be214548f1ed12657e4f8d7a
salih004 <145712148+salih004@users.noreply.github.com>
2026-01-17 22:24:41 -0600

    prep

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/naqi-site/", // <- This fixes all absolute paths
+  base: "/naqi-site/", // Important for GitHub Pages
   plugins: [react()],
 });
