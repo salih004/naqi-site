aa625de67fbeb1e7c59e63e8067a6a7362d7ff4c
salih004 <145712148+salih004@users.noreply.github.com>
2026-01-21 21:09:00 -0600

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
         <img
           src="/naqi-site/images/test1.png"
           alt="Noor Al-Qur’an Institute"
-          className="h-12 md:h-16 w-auto" // responsive: h-12 mobile, h-16 desktop
+          className="h-9 w-auto"
         />
       </motion.a>
 
