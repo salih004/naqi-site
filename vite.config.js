75d6a7886f6492386c2bc9ea6b6d2d1a57e74c04
salih004 <145712148+salih004@users.noreply.github.com>
2026-01-23 10:03:15 -0600

    changed vite configs file base / only for custom domain routing

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/naqi-site/", // Important for GitHub Pages
+  base: "/", // Important for GitHub Pages
   plugins: [react()],
 });
